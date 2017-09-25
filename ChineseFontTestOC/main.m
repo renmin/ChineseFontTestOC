5c5bb4f8ca81f191e0ca5172a80ded742e877285
renmin <renmin129@hotmail.com>
2017-09-25 12:55:45 +0800

    Initial Commit

---
+++ ChineseFontTestOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChineseFontTestOC
+//
+//  Created by minren on 25/09/2017.
+//  Copyright © 2017 minren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
