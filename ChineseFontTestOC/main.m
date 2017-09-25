5530c5d3ca4c895d28a8f3e7932dcf4c1bd7f099
renmin <renmin129@hotmail.com>
2017-09-25 15:55:52 +0800

    create

--- ChineseFontTestOC/main.m
+++ ChineseFontTestOC/main.m
@@ -7,11 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSArray* fontNames = @[@"宋体",@"SongTi SC",@"宋体-简"];
+        for (NSString* fontname in fontNames) {
+            NSLog(@"=============%@=====================",fontname);
+            NSFontDescriptor *descriptor = [NSFontDescriptor fontDescriptorWithName:fontname size:14];
+            
+            NSLog(@"NSFontDescriptor = %@",descriptor);
+            
+            NSLog(@"matchingFont = %@", [descriptor matchingFontDescriptorsWithMandatoryKeys:nil ]);
+
+            
+            NSFont* font = [NSFont fontWithName:fontname size:14];
+            
+            NSLog(@"NSFont = %@",font);
+
+            NSLog(@"localizedNameForFamily = %@",[[NSFontManager sharedFontManager] localizedNameForFamily:fontname face:nil]);
+            
+            NSLog(@"availableMembersOfFontFamily = %@", [[NSFontManager sharedFontManager] availableMembersOfFontFamily:fontname]);
+            
+            
+            if ([descriptor respondsToSelector:@selector(requiresFontAssetRequest)]) {
+                id needDownload = [descriptor performSelector:@selector(requiresFontAssetRequest)];
+                NSLog(@"needDownload = %@",needDownload);
+
+            }
+            
+            
+            
+        }
+        
+        
+        
+        
     }
     return 0;
 }
